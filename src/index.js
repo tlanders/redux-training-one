6822c9ab34ccc9c9a1808b2c8b876a044f7488c5
tlanders <768713+tlanders@users.noreply.github.com>
2020-07-06 14:57:15 -0500

    persisting redux state to local storage

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
+import {persistStore} from "redux-persist";
+import {PersistGate} from "redux-persist/lib/integration/react";
 import {configureStore} from "./store";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const store = configureStore();
+const persistor = persistStore(store);
 
 ReactDOM.render(
-  <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <PersistGate
+                persistor={persistor}
+                loading={<div>Loading...</div>}>
+                <App/>
+            </PersistGate>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
